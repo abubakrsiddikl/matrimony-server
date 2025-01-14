9e20db0aebeee45f62a19dead9433d72162a7502
limon <limonsk026@gmail.com>
2025-01-14 12:20:39 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const morgan = require("morgan");
+const port = process.env.PORT || 5000;
+const app = express();
+
+// use middleware
+app.use(cors());
+app.use(express());
+app.use(morgan("dev"));
+
+// connect to mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfjkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// server root route
+app.get("/", (req, res) => {
+  res.send("matrimoni web server runing");
+});
+
+app.listen(port, () => {
+  console.log(`Matrimoni server running on this port port ${port}`);
+});
