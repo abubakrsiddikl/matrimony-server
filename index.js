130dbc53a97f68d74829dc97bc4d1dc49bfdd483
limon <limonsk026@gmail.com>
2025-01-15 10:03:03 +0600

    jwt verify token verify create complete

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 // verify token middle ware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unathorized access" });
+  }
+  jwt.verify(token, process.env.ACEESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 // connect to mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfjkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -49,6 +62,13 @@ async function run() {
       const token = jwt.sign(user, process.env.ACEESS_TOKEN_SECRET, {
         expiresIn: "15d",
       });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
   } finally {
     // Ensures that the client will close when you finish/error
