9e1f516fd2e680ee3a1ba67da8c6a1ac8974cea3
limon <limonsk026@gmail.com>
2025-04-30 09:33:29 +0600

    initialize payment info

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const SSLCommerzPayment = require('sslcommerz-lts')
-const store_id = process.env.SSL_STORE_ID
-const store_passwd = process.env.SSL_STORE_PASSWORD
-const is_live = false
+const SSLCommerzPayment = require("sslcommerz-lts");
+const store_id = process.env.SSL_STORE_ID;
+const store_passwd = process.env.SSL_STORE_PASSWORD;
+const is_live = false;
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
@@ -496,9 +496,48 @@ async function run() {
     });
 
     // ! ssl commerze payment route
+    const tran_id = new ObjectId().toString();
     app.post("/sslCommerze", async (req, res) => {
       const paymentInfo = req.body;
-      console.log(paymentInfo);
+
+      const data = {
+        total_amount: process.env.PRICE,
+        currency: "BDT",
+        tran_id: "REF123", // use unique tran_id for each api call
+        success_url: "http://localhost:3030/success",
+        fail_url: "http://localhost:3030/fail",
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: paymentInfo.name,
+        cus_email: paymentInfo.email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+      console.log(data)
+      // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      // sslcz.init(data).then(apiResponse => {
+      //     // Redirect the user to payment gateway
+      //     let GatewayPageURL = apiResponse.GatewayPageURL
+      //     res.redirect(GatewayPageURL)
+      //     console.log('Redirecting to: ', GatewayPageURL)
+      // });
     });
 
     // get user stats
